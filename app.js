3286ba76facc574ef95d8df746f8a464a7bbfff6
flame2262 <flame2262@gmail.com>
2015-04-13 00:55:23 +0300

    Small Change from Eyali5

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/nodetest1');
 
-var x= 7;
+var x= 8;
 
 //var routes = require('./routes/index');
 //var game = require('./routes/game');
